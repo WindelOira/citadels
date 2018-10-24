64c5b7a4e9e13b6482effe79f17168de9a18b45d
Windex <windeloira07@gmail.com>
2018-10-24 21:08:24 +0100

    Integrated laratables with datas from db

---
+++ resources/views/admin/partials/page-header.blade.php
@@ -0,0 +1,8 @@
+<!-- Page Header -->
+<div class="page-header row no-gutters py-4">
+	<div class="col-12 col-sm-4 text-center text-sm-left mb-4 mb-sm-0">
+		<span class="text-uppercase page-subtitle">{{ $title }}</span>
+		<h3 class="page-title">{{ $slot }}</h3>
+	</div>
+</div>
+<!--./ Page Header -->
